1bbc3671ed4c9be1e38afe2cf1a1e2bb95548ac3
Rob McGrail <rob@aichi.(none)>
2012-05-05 02:42:14 +1200

    removing unecessary explicit includes of syfony components

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
 require_once __DIR__.'/vendor/silex.phar';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 $app = new Silex\Application();
+
 
 // Templating with twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path'       => __DIR__.'/views',
     'twig.class_path' => __DIR__.'/vendor/twig/lib',
 ));
+
 
 // Music THEORY
 $chords = array('A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#');
@@ -21,6 +19,7 @@ $wisdom = array(
   'Chords... inscrutable fingerful boxes. Pointlessly cruel.',
   'Something else about chords why not?',
 );
+
 
 
 // App
