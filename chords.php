32804de025b0d28b7110ceafe9db7714c6ca9031
Rob McGrail <rob@aichi.(none)>
2012-05-05 12:43:45 +1200

    horrible bugs

--- chords.php
+++ chords.php
@@ -5,9 +5,11 @@ class ChordHelper {
   public $notes = array('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B',);
 
   public function getChord($key, $note) {
-    $offset = array_search($key, $this->notes);
+    $offset = array_search($key, $this->notes, TRUE);
     $notes = array_merge($this->notes, $this->notes);
+
     $parsed_note = $this->parseNote($note);
+
     $k = $parsed_note['note'] + $offset;
     $note = $notes[$k];
     $parsed_note['mode'] = str_replace('seven', '7', $parsed_note['mode']);
@@ -15,14 +17,14 @@ class ChordHelper {
   }
 
   public function parseNote($note) {
-    preg_match('/(\d+)(\w+)/', $note, $matches);
+    preg_match('/(\d+)(\w+)?/', $note, $matches);
     return array('note' => $matches[1], 'mode' => $matches[2]);
   }
 }
 
 
 class Chords extends ChordHelper {
-  public $candidates = array('0', '5', '7seven', '7', 6, '4m', '9m', '2m', '4seven');
+  public $candidates = array('0', '0', '5', '5', '5', '5', '7', '7', '7', '7seven', 6, '4m', '4m', '9m', '2m', '4seven');
 
   public function choose($key, $num) {
     $key = strtoupper($key);
